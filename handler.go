23ff05ed848c1de0a2e27d222b4de36a0a14969a
Dextrouz <rvs.hello@pm.me>
2023-10-18 11:04:30 -0400

    make tags bold in event content

--- handler.go
+++ handler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
+	"text/template"
 	"net/http"
+	"regexp"
 
 	"github.com/ffiat/nostr"
 )
@@ -20,6 +22,11 @@ func (s *Handler) IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/events", http.StatusMovedPermanently)
 }
 
+func BoldHashtags(content string) string {
+	re := regexp.MustCompile(`#(\w+)`)
+	return re.ReplaceAllString(content, "<b>#$1</b>")
+}
+
 func (s *Handler) ListEvents(w http.ResponseWriter, r *http.Request) {
 
 	search := r.URL.Query().Get("pubkey")
@@ -35,6 +42,12 @@ func (s *Handler) ListEvents(w http.ResponseWriter, r *http.Request) {
 	} else {
 		data.Events = s.repository.All()
 	}
+
+    // Apply CSS styling to event content.
+    for _, e := range data.Events {
+        e.Content = BoldHashtags(e.Content)
+        fmt.Println(e.Content)
+    }
 
 	tmpl, err := template.ParseFiles("template/home.html", "template/index.html")
 	if err != nil {
